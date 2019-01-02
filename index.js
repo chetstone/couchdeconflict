2d4ff664329afcb7c6a7bda8dc779f1c71768844
Chester Wood <chet@dewachen.org>
2019-01-01 18:15:17 -0700

    Take a list of conflict docs to delete
    see ~/prj/couchdb/reports/deconflict.js

--- index.js
+++ index.js
@@ -1,151 +1,162 @@
+const request = require('request-promise');
+const async = require('async');
 
-const request = require('request-promise')
-const async = require('async')
-
-const getDoc = function (url) {
+const getDoc = function(url) {
   return request({
     method: 'get',
     json: true,
     url: url,
     qs: {
-      conflicts: true
-    }
-  })
-}
+      conflicts: true,
+    },
+  });
+};
 
 // opts - { url: '' , keep: '', batch: 100, verbose: false, deletions: [] }
-const processDeletions = function (opts) {
-  const deletionAim = opts.deletions.length
-  let deletionCount = 0
-  const progress = function (x, y) {
-    const percent = 100 * x / y
-    const charCount = Math.floor(percent / 2)
-    const p = '[' + '='.repeat(charCount) + '-'.repeat(50 - charCount) + ']'
-    process.stdout.write('  ' + p + ' ' + Math.floor(percent) + '% ' + x + '/' + y + '           \r')
-  }
+const processDeletions = function(opts) {
+  const deletionAim = opts.deletions.length;
+  let deletionCount = 0;
+  const progress = function(x, y) {
+    const percent = (100 * x) / y;
+    const charCount = Math.floor(percent / 2);
+    const p = '[' + '='.repeat(charCount) + '-'.repeat(50 - charCount) + ']';
+    process.stdout.write(
+      '  ' +
+        p +
+        ' ' +
+        Math.floor(percent) +
+        '% ' +
+        x +
+        '/' +
+        y +
+        '           \r'
+    );
+  };
   return new Promise((resolve, reject) => {
     async.doWhilst(
-      function (callback) {
-        const b = opts.deletions.splice(0, opts.batch)
+      function(callback) {
+        const b = opts.deletions.splice(0, opts.batch);
         if (b.length > 0) {
           if (opts.dryrun) {
-            b.forEach((doc) => {
-              console.log('DELETE ' + JSON.stringify(doc))
-            })
-            callback()
+            b.forEach(doc => {
+              console.log('DELETE ' + JSON.stringify(doc));
+            });
+            callback();
           } else {
             const r = {
               method: 'POST',
               url: opts.url.replace(/\/[^/]+$/, '/_bulk_docs'),
               json: true,
-              body: { docs: b }
-            }
+              body: { docs: b },
+            };
             if (opts.verbose) {
-              progress(deletionCount, deletionAim)
+              progress(deletionCount, deletionAim);
             }
-            request(r).then((data) => {
-              deletionCount += b.length
-              callback(null, data)
-            }).catch((e) => {
-              console.log('! ', e)
-              callback(e)
-            })
+            request(r)
+              .then(data => {
+                deletionCount += b.length;
+                console.log(`Deleted ${b.length}`);
+                callback(null, data);
+              })
+              .catch(e => {
+                console.log('! ', e);
+                callback(e);
+              });
           }
         } else {
-          callback(null)
+          callback(null);
         }
       },
-      function () {
-        return (opts.deletions.length > 1)
+      function() {
+        return opts.deletions.length > 1;
       },
-      function (err, data) {
+      function(err, data) {
         if (opts.verbose && !opts.dryrun) {
-          progress(deletionCount, deletionAim)
-          console.log('')
+          progress(deletionCount, deletionAim);
+          console.log('');
         }
         if (err) {
-          reject(err)
+          reject(err);
         } else {
-          resolve(deletionCount)
+          resolve(deletionCount);
         }
       }
-    )
-  })
-}
+    );
+  });
+};
 
 // opts - { url: '' , keep: '', batch: 100, verbose: false }
-const decon = function (opts) {
-  let doc = null
-  const deletions = []
-  let found = false
+const decon = function(opts) {
+  let doc = null;
+  const deletions = [];
+  let found = false;
 
   // defaults
   if (typeof opts.batch === 'undefined') {
-    opts.batch = 100
+    opts.batch = 100;
   }
   if (typeof opts.verbose === 'undefined') {
-    opts.verbose = false
+    opts.verbose = false;
   }
   if (typeof opts.keep === 'undefined') {
-    opts.keep = null
+    opts.keep = null;
   }
   if (typeof opts.keep === 'undefined') {
-    opts.dryrun = false
+    opts.dryrun = false;
   }
 
   // fetch the document with list of conflicts
   if (opts.verbose) {
-    console.log('couchdeconflict')
-    console.log('---------------')
-    console.log('options: ' + JSON.stringify({url: opts.url.replace(/\/\/(.*)@/, '//###:###@'), keep: opts.keep, batch: opts.batch}))
-    console.log('Fetching document')
+    console.log('couchdeconflict');
+    console.log('---------------');
+    console.log(
+      'options: ' +
+        JSON.stringify({
+          url: opts.url.replace(/\/\/(.*)@/, '//###:###@'),
+          keep: opts.keep,
+          batch: opts.batch,
+        })
+    );
   }
-  return getDoc(opts.url).then((d) => {
-    doc = d
 
-    // if the document has no conflicts, there's nothing to do
-    if (!doc._conflicts) {
-      throw new Error('document has no conflicts')
-    }
-    if (opts.verbose) {
-      console.log(doc._conflicts.length + ' conflicts')
-    }
+  // if the document has no conflicts, there's nothing to do
+  /* if (opts.verbose) {
+   *   console.log(doc._conflicts.length + ' conflicts');
+   * }
+   */
 
-    // generate a list of revisions to delete
-    for (let i in doc._conflicts) {
-      if (doc._conflicts[i] !== opts.keep) {
-        deletions.push({_id: doc._id, _rev: doc._conflicts[i], _deleted: true})
-      } else {
-        found = true
-      }
+  // generate a list of revisions to delete
+  opts.docs.forEach(doc => {
+    deletions.push({ _id: doc.id, _rev: doc.key[0], _deleted: true });
+  });
+  // delete the current winner if required
+  if (opts.keep) {
+    if (doc._rev !== opts.keep) {
+      deletions.push({ _id: doc._id, _rev: doc._rev, _deleted: true });
+    } else {
+      found = true;
     }
-    // delete the current winner if required
-    if (opts.keep) {
-      if (doc._rev !== opts.keep) {
-        deletions.push({_id: doc._id, _rev: doc._rev, _deleted: true})
-      } else {
-        found = true
-      }
-    }
+  }
 
-    // if we haven't come across the revision we are supposed to keep, don't proceed
-    if (opts.keep && !found) {
-      throw new Error('Revision ' + opts.keep + ' not found - no conflicts removed')
-    }
+  // if we haven't come across the revision we are supposed to keep, don't proceed
+  if (opts.keep && !found) {
+    throw new Error(
+      'Revision ' + opts.keep + ' not found - no conflicts removed'
+    );
+  }
 
-    // if there are no deletions in the array, we've nothing to do
-    if (deletions.length === 0) {
-      throw new Error('No conflicts found')
-    }
+  // if there are no deletions in the array, we've nothing to do
+  if (deletions.length === 0) {
+    throw new Error('No conflicts found');
+  }
 
-    if (opts.verbose && opts.keep) {
-      console.log('Keeping revision ' + opts.keep)
-    }
+  if (opts.verbose && opts.keep) {
+    console.log('Keeping revision ' + opts.keep);
+  }
 
-    // perform deletions
-    opts.deletions = deletions
-    return processDeletions(opts)
-  })
-}
+  // perform deletions
+  opts.deletions = deletions;
+  return processDeletions(opts);
+};
 
-module.exports = decon
+module.exports = decon;
